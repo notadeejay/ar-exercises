533088f823ac5e66e295ab378a9584531db44e22
Amy <amy@lighthouselabs.ca>
2017-08-29 16:27:51 +0000

    Summing records in AR

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+revenue = Store.sum(:annual_revenue)
+count = Store.count
+average = revenue / count 
+puts "Average annual revenue: #{average}"
+
+stores_mil = Store.where("annual_revenue < ?", 1000000).count
+puts "Number of stores making over 1M: #{stores_mil}"
