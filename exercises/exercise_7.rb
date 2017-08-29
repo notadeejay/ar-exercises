c90a8e262a0691fd6f7444b1b3f362e56088d592
Amy <amy@lighthouselabs.ca>
2017-08-29 17:01:41 +0000

    Adding exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,8 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Please enter a store name"
+store_name = gets.chomp 
+new_store = Store.create(name: store_name)
+
+puts "Error: #{new_store.errors.full_messages}"
